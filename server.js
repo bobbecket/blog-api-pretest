3d2aaa464abd294d21e35f36f2a87fcb1e12522f
Bob Becket <bobbybecket@gmail.com>
2018-06-27 12:50:23 -0700

    first significant commit

---
+++ server.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+//const morgan = require('morgan');
+
+const app = express();
+
+const blogPostsRouter = require('./blogPostsRouter');
+
+//app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use('/blogPosts', blogPostsRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
